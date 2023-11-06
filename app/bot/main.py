c082a2f541f8b0fffbe6d2ea62d2875738992262
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-06 19:19:26 +0400

    update: bot on webhook

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,58 +1,58 @@
-# python imports
-from typing import Union
+# Python imports
+from typing import Dict, Any
 import logging
-import os
-import asyncio
-import time # for test, delete me
+from os import getenv
 
-# telegram imports
-from aiogram import Bot, Dispatcher, types
+# Fastapi imports
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.types.bot_command import BotCommand
-from aiogram.utils.markdown import hbold
-from aiogram.methods.send_message import SendMessage
 
-# fastAPI imports
-from fastapi import FastAPI
-
-# custom foos, classes imports
+# custom foos and classes
 from . import routing
 
-# declare app
-app = FastAPI()
-# declare despatcher for bot
-dp = Dispatcher()
-dp.include_router(router=routing.router)
 
-# declare logging objects
-logging.config.fileConfig('logs/logger_config.conf', disable_existing_loggers=False)
+# logger declaration, logging setup
+logging.config.fileConfig(
+    'logs/logger_config.conf',
+    disable_existing_loggers=False
+)
 logger = logging.getLogger(name="open_pycab_telegram_logger")
 
+# config data
+TOKEN = getenv("TELEGRAM_TOKEN")
+BASE_WEBHOOK_URL = getenv("WEBHOOK_URL")
 
-async def first_try(message):
-    ''' test. work '''
-    await message.answer(text='look')
+# declare fastapi app
+app = FastAPI()
 
-@dp.message(CommandStart())
-async def message_handler(message: types.Message) -> None:
-    ''' answer to start command '''
-    logger.info("first log")
-    asyncio.create_task(first_try(message))
-    await message.answer(text="first try")
+# declare, setup bot
+router = Router()
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+dp.include_router(routing.router)
+
+
+class WebhookData(BaseModel):
+    ''' pydantic class for processing requests '''
+    data: Dict[str, Any]
+
 
 @app.on_event("startup")
 async def run_bot_webhook():
-    ''' before starting app action
-    1. declare bot
-    2. start bot
-    '''
-    bot = Bot(os.getenv("TELEGRAM_TOKEN"), parse_mode=ParseMode.HTML)
-    # skip order of messages
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    ''' set webhook on start of app '''
+    await bot.set_webhook(
+        f"{BASE_WEBHOOK_URL}/{TOKEN}",
+    )
 
 
-# examples
-# https://github.com/MasterGroosha/aiogram-3-guide
+@app.post(f"/{TOKEN}")
+async def feed_update(update: Dict[str, Any]) -> None:
+    try:
+        telegram_update = types.Update(**update)
+#        this variant works too
+#        await dp._process_update(update=types.Update(**update), bot=bot)
+        await dp.feed_webhook_update(bot, telegram_update)
+    except Exception as e:
+        logger.error(f"Error processing update: {e}")    
--- app/bot/routing.py
+++ app/bot/routing.py
@@ -1,13 +1,27 @@
+# Python imports
+import logging
+import asyncio
+
 # aiogram imports
-from aiogram import Router
-from aiogram.filters import Command
+from aiogram import Router, types
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message # for typing
 
-# import custom foos, classes
+# import custom foos, classes, constants
+from .constants import start_text
 
+
+# router for handling signals from bot
 router = Router()
+
 
 @router.message(Command("new_text"))
 async def kek(message: Message):
     ''' remove me '''
     await message.answer("ok, done")
+
+
+@router.message(CommandStart())
+async def message_handler(message: types.Message) -> None:
+    ''' answer to start command '''
+    await message.answer(text=start_text)
